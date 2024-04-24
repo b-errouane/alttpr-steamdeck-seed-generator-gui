3649815d0702ebd742a696770014b7c04dc1931a
b-errouane <Benjamin2002@gmx.de>
2024-04-24 13:53:04 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,14 +18,7 @@ if [ -d "$repo_name" ]; then
         git stash save "Local changes stashed by installer"
     fi
 
-    current_commit=$(git rev-parse HEAD)
     git pull origin master --ff-only
-    latest_commit=$(git rev-parse HEAD)
-    if [ "$current_commit" = "$latest_commit" ]; then
-        echo "No updates available."
-    else
-        echo "Repository updated."
-    fi
 
     if [ -n "$(git stash list)" ]; then
         echo "Applying stashed changes..."
